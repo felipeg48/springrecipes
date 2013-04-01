1df57c4e97e56289336d27b4aed9f0c61d18a7ff
drubio <daniel@webforefront.com>
2013-04-01 01:46:28 -0600

    Added new directory structure for recipes by Chapter

--- Ch2/Recipe_2_1/src/main/java/com/apress/springrecipes/sequence/Main.java
+++ Ch2/Recipe_2_1/src/main/java/com/apress/springrecipes/sequence/Main.java
@@ -1,13 +1,13 @@
 package com.apress.springrecipes.sequence;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = 
-            new ClassPathXmlApplicationContext("beans.xml");
+            new GenericXmlApplicationContext("beans.xml");
 
         SequenceGenerator generator =
             (SequenceGenerator) context.getBean("sequenceGenerator");
--- springintro/src/main/java/com/apress/springrecipes/hello/Main.java
+++ springintro/src/main/java/com/apress/springrecipes/hello/Main.java
@@ -1,13 +1,13 @@
 package com.apress.springrecipes.hello;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = 
-            new ClassPathXmlApplicationContext("beans.xml");
+            new GenericXmlApplicationContext("beans.xml");
 
         HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
         helloWorld.hello();
