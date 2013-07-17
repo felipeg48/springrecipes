915b49e83d9d50e0b664b28aca6bfde49b47bc6f
Daniel <daniel@webforefront.com>
2013-07-17 02:43:06 -0500

    Finished recipes for Ch15 Remoting with Spring-WS

---
+++ Ch15/Recipe_15_11_SpringWS_Client/src/main/java/com/apress/springrecipes/weather/SpringWSInvokerClient.java
@@ -0,0 +1,18 @@
+package com.apress.springrecipes.weather;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class SpringWSInvokerClient {
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+            new GenericXmlApplicationContext("appContext.xml");
+
+        WeatherServiceClient client = (WeatherServiceClient) context.getBean("weatherClient");
+        TemperatureInfo temperature = client.getTodayTemperature("Houston");
+        System.out.println("Min temperature : " + temperature.getMin());
+        System.out.println("Max temperature : " + temperature.getMax());
+        System.out.println("Average temperature : " + temperature.getAverage());
+    }
+}
---
+++ Ch15/Recipe_15_11_SpringWS_Server/src/main/java/com/apress/springrecipes/weather/config/Initializer.java
@@ -0,0 +1,25 @@
+package com.apress.springrecipes.weather.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.context.ContextLoaderListener;
+
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+import javax.servlet.ServletRegistration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+
+public class Initializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext container)
+	throws ServletException {
+	
+	ServletRegistration.Dynamic springws = container.addServlet("springws", new MessageDispatcherServlet());
+	springws.setLoadOnStartup(1);
+	springws.addMapping("/*");
+    }
+}
